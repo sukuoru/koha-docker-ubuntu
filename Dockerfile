c73030259aaa0291d866c18ed48384185c8baf3f
Aldemir Akpinar <aldemir.akpinar@gmail.com>
2021-11-21 21:50:52 +0300

    firt working version

--- Dockerfile
+++ Dockerfile
@@ -12,17 +12,16 @@ RUN curl https://debian.koha-community.org/koha/gpg.asc | apt-key add -
 
 RUN apt-get update && apt-get install -y koha-common && apt-get clean
 
-#RUN sed -i -e 's/USE_MEMCACHED="yes"/USE_MEMCACHED="no"/g' /etc/koha/koha-sites.conf
 RUN sed -i -e 's/DOMAIN=.*/DOMAIN=".mydomain.org"/g' /etc/koha/koha-sites.conf
 RUN sed -i -e 's/INTRASUFFIX=.*/INTRASUFFIX="-admin"/g' /etc/koha/koha-sites.conf
 RUN sed -i -e 's/OPACSUFFIX=.*/OPACSUFFIX="-opac"/g' /etc/koha/koha-sites.conf
 RUN sed -i -e 's/INTRAPORT="80"/INTRAPORT="8080"/g' /etc/koha/koha-sites.conf
 RUN sed -i -e 's/OPACPORT=""/OPACPORT="8081"/g' /etc/koha/koha-sites.conf
 
-# Ignore the memcached error
-RUN koha-translate --install tr-TR || true
 
 RUN a2enmod rewrite && a2enmod cgi && a2enmod deflate
+
+RUN apt-get clean
 
 COPY entrypoint.sh /opt/
 ENTRYPOINT /opt/entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,17 +8,29 @@ chown mysql:mysql /var/run/mysqld
 #Run cron daemon
 /usr/sbin/cron &
 
-if [ ! -z "$LOCAL_MYSQL" ] && [ "$LOCAL_MYSQL" == "true" ];then
+if [ ! -z "$REMOTE_DB" ] && [ "$REMOTE_DB" == "true" ];then
+    echo
+else
     su - mysql -s /bin/bash -c "/usr/sbin/mysqld --basedir=/usr --datadir=/var/lib/mysql --plugin-dir=/usr/lib/x86_64-linux-gnu/mariadb19/plugin --user=mysql --skip-log-error --pid-file=/run/mysqld/mysqld.pid --socket=/var/run/mysqld/mysqld.sock" &
 fi
 
-if [ ! -z "$MYSQL_HOST"] &&
-    [ ! -z "$MYSQL_USER" ] &&
-    [ ! -z "$MYSQL_PASS" ] &&
-    [ ! -z "$MYSQL_PORT" ];then
-    sed -i -e "s///g" /etc/koha/koha-sites.conf
+#if [ ! -z "$MYSQL_HOST"] &&
+#    [ ! -z "$MYSQL_USER" ] &&
+#    [ ! -z "$MYSQL_PASS" ] &&
+#    [ ! -z "$MYSQL_ADMIN_PASS"] &&
+#    [ ! -z "$MYSQL_PORT" ];then
+#    sed -i -e "s///g" /etc/koha/koha-sites.conf
+#fi
+
+if [ ! -z "$NO_MEMCACHED"  ] && [ "$NO_MEMCACHED" == "true" ];then
+    sed -i -e 's/USE_MEMCACHED="yes"/USE_MEMCACHED="no"/g' /etc/koha/koha-sites.conf
+    sed -i -e 's/MEMCACHED_SERVERS=.*/MEMCACHED_SERVERS=""/g' /etc/koha/koha-sites.conf
+else
+    memcached -u memcache -d
 fi
 
 koha-create --create-db library
+# Ignore the memcached error
+RUN koha-translate --install tr-TR || true
 
 /usr/sbin/apachectl  -D FOREGROUND -k start
