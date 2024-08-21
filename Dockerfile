381703b14b8b73906a70036285a0ef2c6bd077e9
sukuoru <justin.neeley@gmail.com>
2024-08-21 22:33:19 +0000

    Converting to use mariadb on a seperate container. Various fixes from errors in Koha System section

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,20 @@
-FROM ubuntu:22.04
+FROM ubuntu:24.04
 #AUTHOR Forked from Aldemir Akpinar <aldemir.akpinar@gmail.com>
 
 ARG DEBIAN_FRONTEND=noninteractive
 #RUN sed -i -e's/ main/ main contrib non-free/g' /etc/apt/sources.list
 
-RUN apt-get update && apt-get install -y tzdata mariadb-client gnupg curl mariadb-server memcached
+RUN apt-get update && apt-get install -y tzdata mariadb-client gnupg curl memcached
+ENV TZ="$TIMEZONE"
 
-RUN echo "deb http://debian.koha-community.org/koha stable main" | tee -a  /etc/apt/sources.list
+RUN echo deb http://debian.koha-community.org/koha 24.05 main | tee -a  /etc/apt/sources.list
+
 
 RUN curl https://debian.koha-community.org/koha/gpg.asc | apt-key add -
 
-RUN apt-get update && apt-get install -y koha-common && apt-get clean
+RUN apt-get update && apt-get install -y koha-common
+RUN apt-get clean \
+&& rm -rf /tmp/* /var/tmp/* /var/cache/apt/archives/* /var/lib/apt/lists/*
 
 RUN sed -i -e 's/DOMAIN=.*/DOMAIN=".mydomain.org"/g' /etc/koha/koha-sites.conf
 RUN sed -i -e 's/INTRASUFFIX=.*/INTRASUFFIX="-admin"/g' /etc/koha/koha-sites.conf
@@ -29,15 +33,19 @@ RUN sed -i -e '/# Reconfigure Apache./,+7d' /usr/sbin/koha-create
 RUN echo "LISTEN 8080" >> /etc/apache2/ports.conf && echo "LISTEN 8081" >> /etc/apache2/ports.conf
 
 RUN a2enmod rewrite && a2enmod cgi && a2enmod deflate
+RUN echo "ServerName localhost" >> /etc/apache2/apache2.conf
 
 COPY entrypoint.sh /opt/
 COPY getpassword.sh /opt/
 
-# Deb post install creates the db files
-RUN rm -rf /var/lib/mysql/*
 # Create a directory to store stateful files
 COPY library.conf /etc/apache2/sites-available
 RUN ln -s /etc/apache2/sites-available/library.conf /etc/apache2/sites-enabled/library.conf
+
+COPY koha-common.cnf /etc/mysql/koha-common.cnf
+COPY koha-conf.xml /etc/koha/sites/library/koha-conf.xml
+COPY log4perl.conf etc/koha/sites/library/log4perl.conf
+COPY rabbitmq-env.conf /usr/share/rabbitmq/rabbitmq-env.conf
 
 # Create koha user manualy and only during docker build
 RUN adduser --no-create-home --disabled-login \
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,34 +2,31 @@
 # Stop on errors
 set -e
 
-mkdir -p /var/run/mysqld
-chown mysql:mysql /var/run/mysqld
-
 #Run cron daemon
 /usr/sbin/cron &
 
-if [ ! -z "$REMOTE_DB" ] && [ "$REMOTE_DB" == "true" ];then
-    # TODO
-    echo
-else
-    echo 'Starting mysql db...'
-    if [ ! -d "/var/lib/mysql/mysql" ];then
-        mysql_install_db
-    fi
-    su - mysql -s /bin/bash -c "/usr/sbin/mysqld --basedir=/usr --datadir=/var/lib/mysql --plugin-dir=/usr/lib/x86_64-linux-gnu/mariadb19/plugin --user=mysql --skip-log-error --pid-file=/run/mysqld/mysqld.pid --socket=/var/run/mysqld/mysqld.sock" &
-fi
+echo "*** Fixing locale error"
 
-# Wait for mysql to start
-sleep 5
+echo "LC_ALL=en_US.UTF-8" >> /etc/environment
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
-#if [ ! -z "$MYSQL_HOST"] &&
-#    [ ! -z "$MYSQL_USER" ] &&
-#    [ ! -z "$MYSQL_PASS" ] &&
-#    [ ! -z "$MYSQL_ADMIN_PASS"] &&
-#    [ ! -z "$MYSQL_PORT" ];then
-#    sed -i -e "s///g" /etc/koha/koha-sites.conf
-#fi
+echo "*** Modifying /etc/mysql/koha-common.cnf and /etc/koha/sites/library/koha-conf.xml"
 
+sed -i -e "s/_DB_HOST_/$DB_HOST/g" /etc/mysql/koha-common.cnf
+sed -i -e "s/_MYSQL_PASSWORD_/$MYSQL_PASSWORD/g" /etc/mysql/koha-common.cnf
+sed -i -e "s/_DB_PORT_/$DB_PORT/g" /etc/mysql/koha-common.cnf
+sed -i -e "s/_MYSQL_USER_/$MYSQL_USER/g" /etc/mysql/koha-common.cnf
+sed -i -e "s+_TIMEZONE_+$TIMEZONE+g" /etc/koha/sites/library/koha-conf.xml
+
+
+echo "Checking if MariaDB is alive..."
+while ! mysqladmin ping -h"$DB_HOST" --silent; do
+    echo "*** Database server still down. Waiting $SLEEP seconds until retry"
+    sleep $SLEEP
+done
+
+echo "Setting MEMCACHED choice..."
 if [ ! -z "$NO_MEMCACHED"  ] && [ "$NO_MEMCACHED" == "true" ];then
     sed -i -e 's/USE_MEMCACHED="yes"/USE_MEMCACHED="no"/g' /etc/koha/koha-sites.conf
     sed -i -e 's/MEMCACHED_SERVERS=.*/MEMCACHED_SERVERS=""/g' /etc/koha/koha-sites.conf
@@ -38,18 +35,36 @@ else
     memcached -u memcache -d
 fi
 
+echo "*** Fixing rabbitmq"
+rabbitmq-server start -detached
+sleep $SLEEP
+rabbitmqctl start_app
+rabbitmqctl add_user koha koha
+rabbitmqctl set_user_tags koha administrator
+rabbitmqctl set_permissions -p / koha ".*" ".*" ".*"
 
+#check for db, if nonexistent then create it
 # Grep exits with 1 when not found
-koha_db_present=$(echo "show databases" | mysql | grep koha_library || true)
+koha_db_present=$(mysql -h $DB_HOST -u root -p$DB_ROOT_PASSWORD -e "show databases;" | grep koha_library || true)
 if [ "$koha_db_present" == "" ];then
     echo "Creating koha library..."
     koha-create --create-db library
+fi
+
+echo "***Fixing db permissions..."
+mysql -h $DB_HOST -u root -p${DB_ROOT_PASSWORD} -e "grant all on koha_$LIBRARY_NAME.* to 'koha_$LIBRARY_NAME'@'%' IDENTIFIED BY 'random';"
+mysql -h $DB_HOST -u root -p${DB_ROOT_PASSWORD} -e "flush privileges;"
+
+echo "*** Setting lockdir..."
+mkdir /var/lock/koha/library
+chown library-koha:library-koha /var/lock/koha/library
 
     if [ ! -z "$INSTALL_LANG" ];then
         # Ignore the memcached error
 	echo "Creating translations...."
         koha-translate --install $INSTALL_LANG || true
     fi
-fi
+
+echo "Finished! Browse to your Koha instance!"
 
 /usr/sbin/apachectl  -D FOREGROUND -k start
